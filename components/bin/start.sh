31dc7336eaa25e6a1cfe9ea762ab79a023a4c5a8
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-01-27 14:13:17 +0100

    Use pyenv to configure broker url

--- components/bin/start.sh
+++ components/bin/start.sh
@@ -14,7 +14,6 @@ do
   envsubst '${ENV} ${DOMAIN} $AUTHENTIC_SUBDOMAIN $COMBO_SUBDOMAIN $COMBO_ADMIN_SUBDOMAIN $FARGO_SUBDOMAIN $HOBO_SUBDOMAIN $PASSERELLE_SUBDOMAIN $WCS_SUBDOMAIN' < "/etc/nginx/conf.d/$COMP.template" > "/etc/nginx/conf.d/$COMP.conf"
 done
 
-envsubst '$RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS $RABBITMQ_PORT' < /etc/hobo-agent/settings.d/custom.template > /etc/hobo-agent/settings.d/custom.py
 envsubst '$DEBUG $LOG_LEVEL $ALLOWED_HOSTS $DB_PORT $DB_HOBO_PASS $DB_PASSERELLE_PASS $DB_COMBO_PASS $DB_FARGO_PASS $DB_WCS_PASS $DB_AUTHENTIC_PASS $RABBITMQ_DEFAULT_USER $RABBITMQ_DEFAULT_PASS $RABBITMQ_PORT $ERROR_MAIL_AUTHOR $ERROR_MAIL_ADDR $SMTP_PORT' < /root/pyenv.template > /home/pyenv.py
 # Needs to be in a directory that can be read by a non-root user, so not "/root"
 chmod 755 /home/pyenv.py
