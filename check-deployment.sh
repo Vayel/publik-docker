43ce22079e49183246973f4577171503f89192a2
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-01-28 10:42:12 +0100

    Hide useless information

--- check-deployment.sh
+++ check-deployment.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# https://doc-publik.entrouvert.com/guide-de-l-administrateur-systeme/installation/deploiement-des-instances/#verification-du-deploiement
+
 check() {
   container=components
   folder=$1
-  docker exec $container ls "$folder"  # Make sure folder exists
+  docker exec $container ls "$folder" >> /dev/null # Make sure folder exists
   if [ $? -ne 0 ]; then
     echo "ERROR: $folder on $container doesn't exist but should"
     exit 1
@@ -22,7 +24,4 @@ check "/var/lib/hobo/tenants"
 check "/var/lib/passerelle/tenants"
 check "/var/lib/wcs"
 
-echo ""
-echo "*******************"
 echo "Deployment looks OK"
-echo "*******************"
