0df18fb7a7693aa7ce297000aa2de59992868611
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-01-30 15:44:51 +0100

    Generate passwords if necessary

--- init-env.sh
+++ init-env.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+function generate_passwords {
+  cp "$1" "$1.tmp"
+  echo "" > "$1"
+  cat "$1.tmp" | while read line; do
+    if [[ "$line" =~ ^[A-Z0-9_]+=$ ]]; then
+      pass=`date +%s | sha256sum | base64 | head -c 32 ; echo`
+      echo "$line$pass" >> "$1"
+      sleep 1  # To increase the date
+    else
+      echo $line >> "$1"
+    fi
+  done
+  rm "$1.tmp"
+}
+
 for file in config.env .env secret.env
 do
   if [ ! -f $file ]; then
     cp "$file.template" $file
+    if [ "$file" == "secret.env" ]; then
+      generate_passwords "$file"
+    fi
   fi
 done
 
