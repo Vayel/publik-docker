fe293e0775f0e0e957bbeca11eb3bcc6d05632e7
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-02-03 15:12:53 +0100

    Rename portal files

--- components/bin/import-site.sh
+++ components/bin/import-site.sh
@@ -8,14 +8,12 @@ if [ -f "$FOLDER/wcs.zip" ]; then
 fi
 sudo -u wcs wcs-manage import_site -d ${WCS_SUBDOMAIN}${ENV}.${DOMAIN} /var/lib/wcs/skeletons/publik.zip
 
-FOLDER=/tmp/site
-
-if [ -f "$FOLDER/combo-site.json" ]; then
+if [ -f "$FOLDER/user-portal.json" ]; then
   echo "Importing user portal..."
-  sudo -u combo combo-manage tenant_command import_site -d ${COMBO_SUBDOMAIN}${ENV}.${DOMAIN} "$FOLDER/combo-site.json"
+  sudo -u combo combo-manage tenant_command import_site -d ${COMBO_SUBDOMAIN}${ENV}.${DOMAIN} "$FOLDER/user-portal.json"
 fi
 
-if [ -f "$FOLDER/combo-agent-site.json" ]; then
+if [ -f "$FOLDER/agent-portal.json" ]; then
   echo "Importing agent portal..."
-  sudo -u combo combo-manage tenant_command import_site -d ${COMBO_ADMIN_SUBDOMAIN}${ENV}.${DOMAIN} "$FOLDER/combo-agent-site.json"
+  sudo -u combo combo-manage tenant_command import_site -d ${COMBO_ADMIN_SUBDOMAIN}${ENV}.${DOMAIN} "$FOLDER/agent-portal.json"
 fi
