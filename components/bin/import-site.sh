4b6a65d5de75fa45832d6c8fdd245cdd17d4183f
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-02-03 15:10:13 +0100

    Make it possible to import forms

--- components/bin/import-site.sh
+++ components/bin/import-site.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-echo "Importing wcs..."
+FOLDER=/tmp/site
+
+if [ -f "$FOLDER/wcs.zip" ]; then
+  echo "Importing wcs..."
+  sudo -u wcs wcs-manage import_site -d ${WCS_SUBDOMAIN}${ENV}.${DOMAIN} "$FOLDER/wcs.zip"
+fi
 sudo -u wcs wcs-manage import_site -d ${WCS_SUBDOMAIN}${ENV}.${DOMAIN} /var/lib/wcs/skeletons/publik.zip
 
 FOLDER=/tmp/site
 
 if [ -f "$FOLDER/combo-site.json" ]; then
-  echo "Importing combo..."
+  echo "Importing user portal..."
   sudo -u combo combo-manage tenant_command import_site -d ${COMBO_SUBDOMAIN}${ENV}.${DOMAIN} "$FOLDER/combo-site.json"
 fi
 
 if [ -f "$FOLDER/combo-agent-site.json" ]; then
-  echo "Importing combo agent..."
+  echo "Importing agent portal..."
   sudo -u combo combo-manage tenant_command import_site -d ${COMBO_ADMIN_SUBDOMAIN}${ENV}.${DOMAIN} "$FOLDER/combo-agent-site.json"
 fi
