cd1e4bd8ea2a1d056101008a118fd5f4251609ff
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-02-04 11:35:42 +0100

    Fix exit after certificate generation

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ docker-compose -f docker-compose.yml -f docker-compose.db.yml build
 docker-compose -f docker-compose.certificates.yml build
 
 docker-compose -f docker-compose.yml pull rabbitmq
+docker-compose -f docker-compose.certificates.yml pull debian
