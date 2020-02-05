33bae8cb5f876632ae561e2869296c77541eed2f
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-02-05 18:09:07 +0100

    Fix service variable value

--- add-org.sh
+++ add-org.sh
@@ -10,15 +10,17 @@ fi
 
 export SLUG=$1
 export TITLE="$2"
+export THEME=publik # TODO: read arg but make sure in cook.sh that the theme is installed before deploying
 DIR="data/sites/$SLUG"
 
 echo "Creating $DIR..."
 echo "Slug: $SLUG"
 echo "Title: $TITLE"
+echo "Theme: $THEME"
 
 mkdir -p $DIR
 DEST="$DIR/hobo-recipe.json.template"
-envsubst '$SLUG $TITLE' < hobo-recipe.json.template > "$DEST.tmp"
+envsubst '$SLUG $TITLE $THEME' < hobo-recipe.json.template > "$DEST.tmp"
 encoding=`file -i "$DEST.tmp" | cut -f 2 -d";" | cut -f 2 -d=`
 iconv -f $encoding -t utf-8 "$DEST.tmp" > $DEST
 rm "$DEST.tmp"
