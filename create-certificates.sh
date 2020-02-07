dbc298daa1005bf2cb6cb7fea37c15de5feffa5d
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-02-07 12:16:50 +0100

    Change default values

--- create-certificates.sh
+++ create-certificates.sh
@@ -8,7 +8,7 @@ docker-compose -f docker-compose.certificates.yml up --no-build --abort-on-conta
 
 echo "***************************************************************"
 echo "SUCCESS: certificates built"
-echo "Please run (need sudo access):"
+echo "Please run (needs sudo access):"
 echo "cd /home/publik/publik-docker/data; sudo zip -r letsencrypt.zip letsencrypt"
 echo "Download letsencrypt.zip and unzip it into the data folder"
 echo "***************************************************************"
