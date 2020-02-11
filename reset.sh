b18389a5228c045a3fe67175e854a1373e93c3e5
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-02-11 11:53:05 +0100

    Fix compose file name

--- reset.sh
+++ reset.sh
@@ -2,5 +2,5 @@
 
 . ./init-env.sh
 
-docker-compose -f docker-compose.yml -f docker-compose.db.yml down -v
-docker-compose -f docker-compose.yml -f docker-compose.db.yml rm -v
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml down -v
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml rm -v
