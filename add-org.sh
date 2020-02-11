b4b53c563047d799a5060abcaffea2603ce9c944
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-02-11 09:38:21 +0100

    Create theme folder with org

--- add-org.sh
+++ add-org.sh
@@ -10,14 +10,25 @@ fi
 
 export SLUG=$1
 export TITLE="$2"
-export THEME=publik # TODO: read arg but make sure in cook.sh that the theme is installed before deploying
 DIR="data/sites/$SLUG"
+THEME_DIR="themes/$SLUG"
 
-echo "Creating $DIR..."
-echo "Slug: $SLUG"
-echo "Title: $TITLE"
-echo "Theme: $THEME"
+echo "Creating $TITLE (slug: $SLUG)..."
 
+if [ -d "$THEME_DIR" ]; then
+  echo "Using existing theme $THEME_DIR"
+else
+  echo "Creating theme in $THEME_DIR..."
+  mkdir -p $THEME_DIR
+  cp -R themes/publik-base/* $THEME_DIR
+  sed -i "s/Publik/$TITLE/g" "$THEME_DIR/desc.xml"
+  sed -i "s/publik-base/$SLUG/g" "$THEME_DIR/desc.xml"
+  sed -i "s/publik/$SLUG/g" "$THEME_DIR/themes.json"
+  sed -i "s/Publik/$TITLE/g" "$THEME_DIR/themes.json"
+  mv "$THEME_DIR/static/publik" "$THEME_DIR/static/$SLUG"
+fi
+
+echo "Creating config files in $DIR..."
 mkdir -p $DIR
 DEST="$DIR/hobo-recipe.json.template"
 envsubst '$SLUG $TITLE $THEME' < hobo-recipe.json.template > "$DEST.tmp"
@@ -29,4 +40,7 @@ DEST="$DIR/wcs-env.sh"
 if [ ! -f "$DEST" ]; then
   cp wcs-env.sh $DEST
 fi
-echo "Please edit $DEST"
+
+echo "Please edit:"
+echo "* Config: $DEST"
+echo "* Theme: $THEME_DIR"
