6f4a31c7418e056641e5c2e3fda2dc2172e89aad
Vincent Lefoulon <vincent.lefoulon@free.fr>
2020-10-12 15:26:16 +0200

    Make it possible to build specific containers only

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@
 
 git log --name-status HEAD^..HEAD > components/commit-build.log
 
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml build
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml build "$@"
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml pull rabbitmq pgadmin mailcatcher
