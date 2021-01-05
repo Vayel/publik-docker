15dd2591510e302748ae905a531a3387b4b8936e
Vincent Lefoulon <vincent.lefoulon@free.fr>
2021-01-05 18:02:36 +0100

    Add IF EXISTS instruction to db dump

--- images/components/bin/dump-db.sh
+++ images/components/bin/dump-db.sh
@@ -8,5 +8,5 @@ if [ -z "$DEST_DIR" ]; then
 fi
 
 for db in authentic2_multitenant combo fargo hobo passerelle wcs; do
-  PGPASSWORD="$PASS_POSTGRES" pg_dump -h "$DB_HOST" -p "$DB_PORT" -U "$DB_ADMIN_USER" $db --clean --create > "$DEST_DIR/$db.sql"
+  PGPASSWORD="$PASS_POSTGRES" pg_dump -h "$DB_HOST" -p "$DB_PORT" -U "$DB_ADMIN_USER" $db --clean --create --if-exists > "$DEST_DIR/$db.sql"
 done
