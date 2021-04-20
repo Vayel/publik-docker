ad4b081819cc7ca5f87379ad8a63f626dc5669b5
Vincent Lefoulon <vincent.lefoulon@free.fr>
2021-04-20 17:46:01 +0200

    N'oblige pas a ce que les conteneurs soient demarres pour faire l'update

--- manage/docker/update.sh
+++ manage/docker/update.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
-set -eu
-
 . ./manage/colors.sh
 
 DEST_DIR=data/previous-packages-versions.txt
 
 rm -f $DEST_DIR
 
-echo "Package versions before update are saved to $DEST_DIR"
+echo_warning "It's recommended to do a backup first."
+echo
+
+echo "If containers are up, package versions before update are saved to $DEST_DIR"
 ./manage/publik/show-packages-versions.sh > $DEST_DIR
+
+set -eu
+
 echo
 git pull --recurse-submodules
-./manage/backup.sh
 # no-cache so that debian packages are updated
 ./manage/docker/build.sh --no-cache
-./manage/docker/down.sh
 
-echo_success "Update finished. Please up the services."
+echo
+echo_success "Update finished. Please restart the services."
+echo "Then, and only then, you might want to prune old Docker images as well: https://docs.docker.com/config/pruning/"
